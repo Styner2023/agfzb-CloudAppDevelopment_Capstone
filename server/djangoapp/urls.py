be9f7259da02ec3e7ab50bdf627f69e39a8f7bd6
Styner <145401330+Styner2023@users.noreply.github.com>
2024-01-20 15:05:19 -0800

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,10 +1,11 @@
 """
 This module defines the URL patterns for the djangoapp application.
 """
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from djangoapp import views
 
 app_name = 'djangoapp'
@@ -19,10 +20,10 @@ urlpatterns = [
     path('dealer/<int:dealer_id>/add-review/', views.add_review, name="add_review"),
     path('dealership/<int:dealer_id>/', views.view_dealership, name='view_dealership'),
     path('login/', views.login_view, name='login'),  # login_view is the view function for login
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('some_path/', views.some_function, name='some_function'),
     path('register/', views.register, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
 # https://kstiner101-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/djangoapp/
 # https://kstiner101-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get
